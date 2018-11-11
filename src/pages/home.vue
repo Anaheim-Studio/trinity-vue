0fc8f83634eceb98d7093ebda59bacf0bf6f45fe
hayato <charexcalibur@gmail.com>
2018-11-11 22:00:22 +0800

    安装必要依赖库

---
+++ src/pages/home.vue
@@ -0,0 +1,25 @@
+<template lang="pug">
+  div.home-container home
+    div.img-container
+      img(class="img" src="../assets/img/bonsai.png")
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style lang="stylus" scoped>
+.home-container
+  font-size 30px
+.img-container
+  width 100%
+  height 400px
+  display flex
+  justify-content center
+  align-items center
+.img
+  width 700px
+  height 100%  
+</style>
