55ce11424f8383afdd1d4f33ec60bef069c34806
hayato <charexcalibur@gmail.com>
2018-11-17 12:52:22 +0800

    修复json格式问题

--- src/common/api/api.js
+++ src/common/api/api.js
@@ -9,8 +9,23 @@ class Api {
     console.log(ws)
 
     ws.onopen = (evt) => {
-      let data = '{"type":"InitRenderer","req":{"rid":1,"sid":0,"params":{"protocol":"tcp.ws","rendertype":"SimpleRenderer","fileid":"UVFData@./Bonsai-SCANLINE-132-lz4.uvf","ioendpoint":"tcp.prefixed:127.0.0.1:6678","streamingparams":{"xres":800,"yres":800}}}}'
-      ws.send(data)
+      let data = {
+        "type":"InitRenderer",
+        "req":{
+          "rid":1,"sid":0,
+          "params":{
+            "protocol":"tcp.ws",
+            "rendertype":"SimpleRenderer",
+            "fileid":"UVFData@./Bonsai-SCANLINE-132-lz4.uvf",
+            "ioendpoint":"tcp.prefixed:127.0.0.1:6678",
+            "streamingparams":{
+              "xres":800,
+              "yres":800
+            }
+          }
+        }
+      }
+      ws.send(JSON.stringify(data))
     }
     // ws.onopen()
   }
